dc30f93b99ff7eb60339c4c7d292dd256402118b
zezouain <eduardo_zouain@hotmail.com>
2022-02-08 14:55:11 -0300

    landing page, firebase conf, login autentication, page room and questions list

---
+++ src/pages/Home.tsx
@@ -0,0 +1,84 @@
+
+import { useNavigate } from 'react-router-dom';
+import { FormEvent, useState } from 'react';
+
+import { database } from '../services/firebase';
+
+import illustrationImg from '../assets/images/illustration.svg';
+import logoImg from '../assets/images/logo.svg';
+import googleIconImg from '../assets/images/google-icon.svg';
+
+import '../styles/auth.scss';
+import { Button } from '../components/Button';
+
+import { useAuth } from '../hooks/useAuth';
+
+
+export function Home() {
+  const navigate = useNavigate();
+  const { user, signInWithGoogle } = useAuth();
+  const [roomCode, setRoomCode] = useState('');
+
+  async function handleCreateRoom() {
+    if (!user) {
+      await signInWithGoogle()
+    }
+
+
+    navigate('/rooms/new');
+  }
+
+  async function handleJoinRoom(event: FormEvent) {
+    event.preventDefault();
+
+    if (roomCode.trim() === '') {
+      return;
+    }
+
+    const roomRef = await database.ref(`rooms/${roomCode}`).get();
+
+    if (!roomRef.exists()) {
+      alert('Room does not exists.');
+      return;
+    }
+
+    navigate(`/rooms/${roomCode}`);
+  }
+
+  return (
+    <div id="page-auth">
+      <aside>
+        <img src={illustrationImg} alt="illustration simbolizando perguntas e respostas" />
+        <strong>Crie salas de Q&amp;A ao-vivo</strong>
+        <p>Tire as duvidas da sua audiência em tempo-real</p>
+      </aside>
+      <main>
+
+        <div className="main-content">
+          <img src={logoImg} alt="logo" />
+          <button onClick={handleCreateRoom} className="create-room">
+            <img src={googleIconImg} alt="Logo do google" />
+            Crie sua sala com o google
+          </button>
+
+          <div className='separator'>
+            Ou entre em uma sala
+          </div>
+
+          <form onSubmit={handleJoinRoom}>
+            <input
+              type="text"
+              placeholder="Digite o código da sala"
+              onChange={event => setRoomCode(event.target.value)}
+              value={roomCode} />
+
+            <Button type="submit">
+              Entrar na sala
+            </Button>
+          </form>
+        </div>
+      </main>
+
+    </div>
+  )
+}
---
+++ src/services/firebase.ts
@@ -0,0 +1,30 @@
+// Import the functions you need from the SDKs you need
+// import firebase from "firebase/app";
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import 'firebase/compat/database';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+import 'firebase/auth'
+import 'firebase/database'
+
+
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+const auth = firebase.auth();
+const database = firebase.database();
+
+
+export { firebase, auth, database }
